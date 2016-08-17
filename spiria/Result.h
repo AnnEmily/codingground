4681c3413b74090b812145d8d05b77150138205a
AnnEmily <ann.emily.gregoire@gmail.com>
2016-08-17 19:31:49 -0400

    Created by Coding Ground Client

---
+++ spiria/Result.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <vector>
+#include "ResultManager.h"
+
+using namespace std;
+
+class Result
+{
+public:
+
+    enum OpIs
+    {
+        opIsInit,
+        opIsPlus,
+        opIsMinus,
+        opIsTimes,
+        opIsDivide
+    };
+    
+    Result (OpIs operation, vector<int> values, int target, int cumulated, 
+            Result *previous, ResultManager *resultManager);
+    ~Result();
+    
+    bool   GoDeeper    () ;
+    string GetOpString () {return m_opString;} ;
+    int    GetUid      () {return m_uid;     } ;
+    
+private:
+    Result        *m_previous;
+    ResultManager *m_manager;
+    
+    ResultManager::Verbosity     m_verbosity;
+    ResultManager::StopCondition m_stopCondition;
+    int                          m_uid;
+    
+    vector<int>   m_available;
+    int           m_target;
+    int           m_cumulated;
+    int           m_operand;
+    
+    OpIs          m_operation;
+    string        m_opString ;
+    
+    Result       *m_plus;
+    Result       *m_minus;
+    Result       *m_times;
+    Result       *m_divide;
+} ;
