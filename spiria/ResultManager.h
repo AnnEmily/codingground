6171510906fcf3341e3ca8863e53d014295837a6
AnnEmily <ann.emily.gregoire@gmail.com>
2016-08-18 14:45:33 -0400

    Update ResultManager.h

--- spiria/ResultManager.h
+++ spiria/ResultManager.h
@@ -28,8 +28,9 @@ public:
     void          SetResult         (int result, string opString);
     bool          WasTargetFound    () {return m_bestDiff == 0;} ;
     int           GetNewUid         () ;
-    Verbosity     GetVerbosity      () {return m_verbosity;} ;
     StopCondition GetStopCondition  () {return m_stopCondition;} ;
+    int           GetSolutionCount  () {return m_solutionCount;} ;
+    int           GetOperationCount () {return m_operationCount;} ;
     string        GetClosestResult  (int & result) ;
     void          PrintNodeCreated  (int uid, Result *previousNode, int cumulated, string operation, int operand, vector<int> nextValues) ;
     void          PrintNodeDropped  (int uid) ;
@@ -38,9 +39,11 @@ public:
 private:
     int           m_uid ;
     Verbosity     m_verbosity ;
-    StopCondition m_stopCondition;
-    int           m_target;
-    int           m_bestDiff;
-    int           m_bestResult;
-    string        m_opString;
+    StopCondition m_stopCondition ;
+    int           m_target ;
+    int           m_bestDiff ;
+    int           m_bestResult ;
+    string        m_opString ;
+    int           m_solutionCount ;
+    int           m_operationCount ;
 } ;
