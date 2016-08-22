da82e91a341e095d0c9e2288f03fc191f6757e9e
AnnEmily <ann.emily.gregoire@gmail.com>
2016-08-22 14:53:31 -0400

    Created by Coding Ground Client

---
+++ mirametrix/main.cpp
@@ -0,0 +1,67 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool AsciiToNum (char *src, int &num)
+{
+    num = 0 ;
+    vector<int> digits ;
+    char *ptr = src ;
+    
+    while (*ptr != 0)
+    {
+        int val = static_cast<int>(*ptr - '0') ;
+        
+        if (val < 0 || val > 9)
+            return false ;
+        else
+            digits.push_back (val) ;
+            
+        ptr++ ;
+    } ;
+    
+    reverse (digits.begin(), digits.end()) ;
+    int mult = 1 ;
+    
+    for (int digit : digits)
+    {
+        num  += digit * mult ;
+        mult *= 10 ;
+    }
+    
+    return true ;
+}
+
+int main (int argc, char **argv)
+{
+    if (argc < 3)
+    {
+        cout << " Need two numbers" << endl;
+        return 1;
+    }
+    
+    int num ;
+    
+    if (!AsciiToNum(argv[1], num))
+    {
+        cout << argv[1] << " is not an integer" << endl ;
+        return 1 ;
+    }
+     
+    int den ;
+    
+    if (!AsciiToNum(argv[2], den))
+    {
+        cout << argv[2] << " is not an integer" << endl ;
+        return 1 ;
+    }
+    
+    if (den != 0)
+    {
+        cout << " Remainder is " << 3 % 5 << endl;
+    }
+   
+   return 0;
+}
